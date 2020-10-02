633d802daff3b42ab2e16fdaa355c2ad5cbba417
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2020-10-02 17:48:47 +0400

    Category model added

--- foodstories/inventory/models.py
+++ foodstories/inventory/models.py
@@ -12,12 +12,19 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     text = models.TextField(max_length=3000)
-    added_at = models.DateTimeField('date published')
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True, related_name='recipe')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, db_index=True, related_name='recipe')
 
 
 class Comments(models.Model):
     recipes = models.ForeignKey('Recipe', on_delete=models.CASCADE, db_index=True, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, db_index=True, related_name='comments')
     text = models.TextField(max_length=1000)
-    added_at = models.DateTimeField('date published')
+    added_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    tagname = models.CharField(max_length=80)
+    
