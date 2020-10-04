16730a7119619cd7762b2fd1ef3d44016f8b5967
Murad <xomurad41@gmail.com>
2020-10-04 18:56:42 +0400

    index page and pagination added

--- foodstories/inventory/urls.py
+++ foodstories/inventory/urls.py
@@ -7,6 +7,7 @@ app_name = 'inventory'
 from inventory.views import RecipeView
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index-page'),
     path('recipes', views.RecipeView.as_view(), name='recipe-list'),
     path('recipes/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe')
 ]
--- foodstories/inventory/views.py
+++ foodstories/inventory/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
-from inventory.models import Recipe, Comments
+from inventory.models import Recipe, Comments, User
 
 
 class RecipeView(ListView):
@@ -11,11 +11,11 @@ class RecipeView(ListView):
     context_object_name = 'recipe_list'
     queryset = Recipe.objects.order_by('-id')
     template_name = 'recipes.html'
-    # paginate_by = 3
+    paginate_by = 3
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['murad'] = 'Murad'
+        context['author'] = User.objects.first()
         return context
 
 class RecipeDetailView(DetailView):
@@ -26,4 +26,20 @@ class RecipeDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        return context
+
+class IndexView(ListView):
+    model = Recipe
+    # template_name_list = [['index.html', 'single.html']]
+    queryset = Recipe.objects.order_by('-id')[:4]
+    context_object_name = 'index_recipes'
+    # template_name = 'index.html'
+
+    def get_template_names(self):
+        return ['index.html']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['first'] = Recipe.objects.order_by('-id')[0]
+        context['author'] = User.objects.first()
         return context
